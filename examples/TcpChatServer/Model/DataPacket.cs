ab59dd5234694599abada594b593e48d1ab71ca3
M Arslan Riaz <arslanriaz96developer@gmail.com>
2020-05-23 22:55:37 +0500

    First Commit

---
+++ examples/TcpChatServer/Model/DataPacket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TcpChatServer.Model
+{
+    public class DataPacket
+    {
+        public DateTime Date { get; set; }
+        public string Lat { get; set; }
+        public string Lan { get; set; }
+        public string Lat1 { get; set; }
+        public string Lan1 { get; set; }
+        public string Speed { get; set; }
+        public string Course { get; set; }
+        public string Height { get; set; }
+        public string Stats { get; set; }
+        public string Crc16 { get; set; }
+        public string Hdop { get; set; }
+        public string Input { get; set; }
+        public string Output { get; set; }
+        public string Adc { get; set; }
+        public string IButton { get; set; }
+        public string Params { get; set; }
+        public string ThreadId { get; set; }
+        public PacketType PacketType { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+
+}
---
+++ examples/TcpChatServer/Model/DeviceRawData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TcpChatServer.Model
+{
+    public class DeviceRawData
+    {
+        public string RawData { get; set; }
+        public string DeviceLogin { get; set; }
+        public bool IsSync { get; set; } = false;
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+    }
+}
