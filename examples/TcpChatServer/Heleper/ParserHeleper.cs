d2e98d9333edee9f22b9e76aca637645ef2f287c
M Arslan Riaz <arslanriaz96developer@gmail.com>
2020-06-10 02:50:37 +0500

    Updated Server

---
+++ examples/TcpChatServer/Heleper/ParserHeleper.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using TcpChatServer.Model;
+
+namespace TcpChatServer.Heleper
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToDateTime(this string s,
+            string format = "ddMMyy", string cultureString = "tr-TR")
+        {
+            try
+            {
+                var r = DateTime.ParseExact(
+                    s: s,
+                    format: format,
+                    provider: CultureInfo.GetCultureInfo(cultureString));
+                return r;
+            }
+            catch (FormatException)
+            {
+                throw;
+            }
+            catch (CultureNotFoundException)
+            {
+                throw; // Given Culture is not supported culture
+            }
+        }
+
+        public static DateTime ToDateTime(this string s,
+            string format, CultureInfo culture)
+        {
+            try
+            {
+                var r = DateTime.ParseExact(s: s, format: format,
+                    provider: culture);
+                return r;
+            }
+            catch (FormatException)
+            {
+                throw;
+            }
+            catch (CultureNotFoundException)
+            {
+                throw; // Given Culture is not supported culture
+            }
+
+        }
+
+    }
+
+    public class ParserHeleper
+    {
+        private void fileWrite(string file)
+        {
+
+            string path = @"C:\Users\Administrator\Desktop\Newfolder\data2.txt";
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(file);
+                    DataParse(file);
+                }
+            }
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(file);
+                DataParse(file);
+            }
+        }
+        private static string AddChunkSeparator(string str, int chunk_len, char separator)
+        {
+            if (str == null || str.Length < chunk_len)
+            {
+                return str;
+            }
+
+            int count = 0;
+            StringBuilder builder = new StringBuilder();
+            for (var index = 0; index < str.Length; index += chunk_len)
+            {
+                builder.Append(str, index, chunk_len);
+                if (count < 2)
+                {
+                    builder.Append(separator);
+                }
+                count++;
+            }
+
+            count = 0;
+            return builder.ToString();
+        }
+        private static DataPacket DataParse(string data)
+        {
+            var response = data.Split(";");
+            if (response.Length > 5)
+            {
+                var dataPacket = new DataPacket();
+                dataPacket.CreatedAt = DateTime.Now;
+                dataPacket.PacketType = PacketType.D;
+                dataPacket.ThreadId = "1";
+                dataPacket.Date = response[0].Trim().ToDateTime(format: "ddMMyy");
+                dataPacket.Date =
+                    dataPacket.Date.Date.Add(DateTime.ParseExact(AddChunkSeparator(response[1].Trim(), 2, ':'), "HH:mm:ss", CultureInfo.InvariantCulture)
+                        .TimeOfDay);
+                dataPacket.Lat = response[2];
+                dataPacket.Lat1 = response[3];
+                dataPacket.Lan = response[4];
+                dataPacket.Lan1 = response[5];
+                dataPacket.Speed = response[6];
+                dataPacket.Course = response[7];
+                dataPacket.Height = response[8];
+                dataPacket.Stats = response[9];
+                dataPacket.Hdop = response[10];
+                dataPacket.Input = response[11];
+                dataPacket.Output = response[12];
+                dataPacket.Adc = response[13];
+                dataPacket.IButton = response[14];
+                dataPacket.Params = response[15];
+                return dataPacket;
+            }
+            return null;
+        }
+      
+        public void DataPacketParser(string data)
+        {
+            fileWrite(data);
+
+            // var dataPacket = new DataPacket();
+            //type
+            //data = "#B#281119;073044;5351.7216;N;02741.3750;E;0.000;232.336;266.875;6;2.730;NA;NA;NA;NA;100_521347:1:521246,100_521129:1:1,100_521050:1:1,100_521106:3:Technoton|281119;073043;5351.7216;N;02741.3750;E;0.000;62.664;265.750;6;2.730;NA;NA;NA;NA;100_521347:1:521246"
+            //    + ",100_158:1:300,100_521055:1:300,100_521056:1:4,100_521488:1:4194304,100_521050:1:1,100_521129:1:1,100_521072_21.2:1:130,100_521106:3:Technoton | 281119; 072947; 5351.7216; N; 02741.3750; E; 0.000; 253.008; 270.125; 6; 2.740; NA; NA; NA; NA; 100_521347:1:521246 | 281119; 07294"
+            //    + "6; 5351.7216; N; 02741.3750; E; 0.000; 253.008; 270.125; 6; 2.740; NA; NA; NA; NA; 100_521347:1:521246 | 281119; 072945; 5351.7216; N; 02741.3750; E; 0.000; 253.008; 270.125; 6; 2.740; NA; NA; NA; NA; 100_521347:1:521246,100_521106:3:Technoton | 281119; 072944; 5351.7216; N; 02741.3750; E; 0.00"
+            //    + "0; 246.789; 270.375; 6; 2.740; NA; NA; NA; NA; 100_521347:1:521246,100_521129:1:1,100_521050:1:1,100_521106:3:Technoton | F48";
+
+            //  data = "#D#271119;212102;5351.6080;N;02741.1311;E; 0.000;61.266;131.125;9;1.500;NA;NA;NA;NA;101_521347:1:521246,101_158:1:298,101_521055:1:298,101_521055_2.9:1:297,101_521056:1:1;DED4";
+
+            //if (data.Contains("#D#"))
+            //{
+            //    var response = data.Split("#")[2];
+            //    dataPacket.Crc16 = response.Split(";")[16];
+            //}
+            //else if (data.Contains("#B#"))
+            //{
+            //    var response = data.Split("#")[2].Split("|");
+            //    foreach (var item in response)
+            //    {
+            //        var response2 = DataParse(item);
+            //    }
+            //}
+            //else
+            //{
+
+            //}
+
+            //length
+        }
+    }
+}
---
+++ examples/TcpChatServer/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using NetCoreServer;
+using TcpChatServer.Repositroy;
+using TcpChatServer.Model;
+
+namespace TcpChatServer
+{
+
+    class ClientManagement
+    {
+        public string LoginId { get; set; }
+        public Guid NewKey { get; set; }
+        public Guid OldKey { get; set; }
+    }
+    class AccuguageSession : TcpSession
+    {
+
+        List<ClientManagement> tcpClient = new List<ClientManagement>();
+        public AccuguageSession(TcpServer server) : base(server) {
+           
+        }
+        
+
+        private void fileWrite(string file,Guid clientId)
+        {
+            try
+            {
+
+                string path = @"C:\Users\Administrator\Desktop\ArslanTCP\ArslanTCP.txt";
+                // This text is added only once to the file.
+                if (!File.Exists(path))
+                {
+                    //var response = DataRepositroy.IsertRaw("Server=173.248.132.203,1533; Initial Catalog=accuguage_db; User Id = sa; Password=7gUSS@nKH;", new DeviceRawData { DeviceLogin = this.DeviceCurrentLogin, RawData = file });
+                    //Console.WriteLine(response);
+                    // Create a file to write to.
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        var response = DataRepositroy.IsertRaw("Server=173.248.132.203,1533; Initial Catalog=accuguage_TCP_db; User Id = sa; Password=7gUSS@nKH;", new DeviceRawData { DeviceLogin = this.tcpClient.FirstOrDefault(x => x.NewKey == Id).LoginId, RawData = file });
+                        Console.WriteLine(response);
+                        sw.WriteLine(file);
+
+                    }
+                }
+          
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                    Console.WriteLine($"TCP Connected Client{tcpClient.Count}");
+                var response = DataRepositroy.IsertRaw("Server=173.248.132.203,1533; Initial Catalog=accuguage_TCP_db; User Id = sa; Password=7gUSS@nKH;", new DeviceRawData { DeviceLogin = this.tcpClient.FirstOrDefault(x=>x.NewKey==Id).LoginId, RawData = file });
+                Console.WriteLine(response);
+                sw.WriteLine(file);
+                
+            }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        protected override void OnConnected()
+        {
+            Console.WriteLine($" TCP session with Id {Id} connected!");
+            SendAsync("#AL#1\r\n");
+        }
+
+        protected override void OnDisconnected()
+        {
+            var response=tcpClient.FirstOrDefault(x => x.NewKey == Id);
+            if (response != null)
+            {
+                response.OldKey = response.NewKey;
+            }
+            Console.WriteLine($" TCP session with Id {Id} disconnected!");
+        }
+
+        protected override void OnReceived(byte[] buffer, long offset, long size)
+        {
+
+            Console.WriteLine("connect Client Id" + this.Id);
+            string message = Encoding.UTF8.GetString(buffer, (int)offset, (int)size);
+          if(message.Contains("#L#"))
+            {
+                //message.Contains("#L#")
+                //check if key is contain
+               var response=tcpClient.FirstOrDefault(x=>x.LoginId.Equals(message.Split(";")[1]));
+                if (response == null)
+                {
+                    tcpClient.Add(new ClientManagement
+                    {
+                        NewKey = Id,
+                        LoginId = message.Split(";")[1]
+                    });
+                }
+                else 
+                {
+                    
+                 if (response.LoginId.Equals(message.Split(";")[1]))
+                    {
+                        response.NewKey = Id;
+                    }
+                    //get index
+                    tcpClient[tcpClient.IndexOf(response)] = response;
+                }
+                //check 
+                //  tcpClient.Values.
+
+                //check if the key is contain but the value is the same
+
+
+            }
+            fileWrite(message,Id);
+            Console.WriteLine("Incoming: " + message);
+            string str = "";
+            if (message.Contains("#L#"))
+            {
+                if (message.Contains("NA"))
+                {
+                    //check device 
+                    //get current device id
+                    str = "#AL#01\r\n";
+                }
+                else
+                {
+                    str = "#AL#1\r\n";
+                }
+
+            }
+            else if (message.Contains("#D#"))
+            {
+                if (message.Split("#")[2].Split(";").Length < 15)
+                {
+                    str = "#AD#-1\r\n";
+                }
+                else
+                {
+                    str = "#AD#1\r\n";
+                }
+
+            }
+            else if (message.Contains("#B#"))
+            {
+                str = "#AB#3\r\n";
+            }
+            else
+            {
+                str = "#AL#0\r\n";
+            }
+            SendAsync(str);
+          
+            Console.WriteLine(str);
+          
+
+            if (message == "!")
+                Disconnect();
+        }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"TCP session caught an error with code {error}");
+        }
+    }
+
+    class AccuguageServer : TcpServer
+    {
+        public AccuguageServer(IPAddress address, int port) : base(address, port) {}
+
+        protected override TcpSession CreateSession() { return new AccuguageSession(this); }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"TCP server caught an error with code {error}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TCP server port
+            int port = 8999;
+            if (args.Length > 0)
+                port = int.Parse(args[0]);
+
+            Console.WriteLine($"TCP server port: {port}");
+
+            Console.WriteLine();
+
+            // Create a new TCP chat server
+            //173.248.132.203
+            var server = new AccuguageServer(IPAddress.Parse("173.248.132.203"), port);
+
+            // Start the server
+            Console.Write("Server starting...");
+            server.Start();
+            Console.WriteLine("Done!");
+
+            Console.WriteLine("Press Enter to stop the server or '!' to restart the server...");
+
+            // Perform text input
+            for (;;)
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                // Restart the server
+                if (line == "!")
+                {
+                    Console.Write("Server restarting...");
+                    server.Restart();
+                    Console.WriteLine("Done!");
+                    continue;
+                }
+
+                // Multicast admin message to all sessions
+                line = "(admin) " + line;
+                server.Multicast(line);
+            }
+
+            // Stop the server
+            Console.Write("Server stopping...");
+            server.Stop();
+            Console.WriteLine("Done!");
+        }
+    }
+}
